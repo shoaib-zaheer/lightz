193e18d84090e4c4b244b2b20a3a5dfdc7295a5d
Mykola Marenich <marenich.mykola@gmail.com>
2020-07-10 19:49:29 +0200

    ver 1.0

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LightZ project starter page
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/HackYourFutureBelgium/class-7-8-projects/issues/1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project issue
         </a>
       </header>
     </div>
