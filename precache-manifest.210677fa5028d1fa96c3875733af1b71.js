fa14626cce0b310b5022ba73859e9190ea5c88f7
Mykola Marenich <marenich.mykola@gmail.com>
2020-07-10 19:17:01 +0200

    Updates

---
+++ precache-manifest.210677fa5028d1fa96c3875733af1b71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef5ff0018308e6f4f683078b8e98223",
+    "url": "/lightz/index.html"
+  },
+  {
+    "revision": "2fe21a1ab789ecdad083",
+    "url": "/lightz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6216774001918e8914ff",
+    "url": "/lightz/static/js/2.81e75736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lightz/static/js/2.81e75736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe21a1ab789ecdad083",
+    "url": "/lightz/static/js/main.5f4cdd91.chunk.js"
+  },
+  {
+    "revision": "fccca426ffc4befbf05c",
+    "url": "/lightz/static/js/runtime-main.ee922a4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lightz/static/media/logo.5d5d9eef.svg"
+  }
+]);
